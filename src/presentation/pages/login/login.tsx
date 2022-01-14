e8b05de6b5ad31c80b3ff443bdffa63e07140aaf
Eduardo Lima <dlima78@hotmail.com>
2022-01-14 10:38:12 -0300

    chore: install Storybook

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import * as S from './login.styled'
+import GlobalStyles from '@/presentation/styles/global'
+import theme from '@/presentation/styles/themes'
+import { ThemeProvider } from 'styled-components'
 
 const Login: React.FC = () => {
   return (
-    <S.LoginWrapper>Ola</S.LoginWrapper>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <S.LoginWrapper>Ola</S.LoginWrapper>
+    </ThemeProvider>
   )
 }
 
