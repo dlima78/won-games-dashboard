1aaacb849663f1c7c3d6ea2c2fb5451f3993804f
Eduardo Lima <dlima78@hotmail.com>
2022-01-20 14:37:40 -0300

    chore: automate component creation

---
+++ plopfile.js
@@ -0,0 +1,33 @@
+module.exports = function (plop) {
+  // controller generator
+  plop.setGenerator('component', {
+    description: 'application component logic',
+    prompts: [{
+      type: 'input',
+      name: 'name',
+      message: 'component name please'
+    }],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/presentation/components/{{dashCase name}}/index.tsx',
+        templateFile: 'templates/component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/presentation/components/{{dashCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: 'src/presentation/components/{{dashCase name}}/{{dashCase name}}.styled.tsx',
+        templateFile: 'templates/styled.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: 'tests/presentation/components/{{dashCase name}}/{{dashCase name}}.spec.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      }
+    ]
+  })
+}
