55436dd7d0a5e5ac03ff34fc303f218b41b67719
Eduardo Lima <dlima78@hotmail.com>
2022-02-01 20:59:40 -0300

    refactor: move URL to a factory method

--- tests/data/usecases/authentication/remote-authentication.spec.ts
+++ tests/data/usecases/authentication/remote-authentication.spec.ts
@@ -7,7 +7,7 @@ type SutTypes = {
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
