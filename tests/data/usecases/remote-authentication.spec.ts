326e2bca9b1df00d82ec6907abb2f3e01f9e6dbd
Eduardo Lima <dlima78@hotmail.com>
2022-02-02 14:49:42 -0300

    test: ensure RemoteAuthentication throws UnexpectedError on 500

--- tests/data/usecases/remote-authentication.spec.ts
+++ tests/data/usecases/remote-authentication.spec.ts
@@ -51,4 +51,13 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthenticationParams())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('should throws UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.auth(mockAuthenticationParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
