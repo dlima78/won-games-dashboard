2572a8eac0f4eabc9460ae54f95c241657d5617f
Eduardo Lima <dlima78@hotmail.com>
2022-02-03 08:47:58 -0300

    feat ensure AxiosHttpClient calls axios with correct verb

--- src/infra/http/axios-http-client.ts
+++ src/infra/http/axios-http-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 export class AxiosHttpClient {
   async post (params: HttpRequest<any>): Promise<void> {
-    await axios(params.url)
+    await axios.post(params.url)
   }
 }
