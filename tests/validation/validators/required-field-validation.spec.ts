0b7e57ca2480fe6b3797b5e9e0fae36e35eb94b3
Eduardo Lima <dlima78@hotmail.com>
2022-02-23 09:57:11 -0300

    refactor: remove code duplication

--- tests/validation/validators/required-field-validation.spec.ts
+++ tests/validation/validators/required-field-validation.spec.ts
@@ -2,26 +2,17 @@ import { RequiredFieldValidation } from '@/validation/validators'
 import { RequiredFieldError } from '@/validation/errors/required-field-error'
 import faker from '@faker-js/faker'
 
-type SutTypes = {
-  sut: RequiredFieldValidation
-}
-
-const makeSut = (): SutTypes => {
-  const sut = new RequiredFieldValidation('email')
-  return {
-    sut
-  }
-}
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
 
 describe('RequiredFieldValidation', () => {
   test('should return error if field is empty', () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
   })
 
   test('should return falsy if field is not empty', () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     const error = sut.validate(faker.random.word())
     expect(error).toBeFalsy()
   })
