c66bc0ab90a9b19f9a38d9ac66daee051eb9245b
Eduardo Lima <dlima78@hotmail.com>
2022-03-10 19:04:24 -0300

    test: ensure SignIn does not present error if form is valid

---
+++ tests/e2e/cypress/integration/sign-in.ts
@@ -0,0 +1,37 @@
+/// <reference path="../support/index.d.ts" />
+import faker from '@faker-js/faker'
+describe('Cypress TS', () => {
+  beforeEach(() => {
+    cy.visit('sign-in')
+  })
+
+  it('should load with correct initial state', () => {
+    cy.findByPlaceholderText(/email/i).should('exist')
+    cy.findByPlaceholderText(/senha/i).should('exist')
+    cy.findByRole('link', { name: /esqueceu a senha?/i}).should('exist')
+    cy.findByRole('button', { name: /entrar/i}).should('have.attr', 'disabled')
+    cy.findByRole('link', { name: /criar conta/i}).should('exist')
+  })
+
+  it('should present error if form is invalid', () => {
+    cy.findByPlaceholderText(/email/i)
+    .type(faker.random.word())
+    .blur()
+    cy.findByText(/campo email inválido/i).should('exist')
+    cy.findByPlaceholderText(/senha/i)
+    .type(faker.random.alphaNumeric(3))
+    .blur()
+    cy.findByText(/o campo precisa ter no minimo 5 caracteres/i).should('exist')
+  })
+
+  it('should not present error message if form is valid', () => {
+    cy.findByPlaceholderText(/email/i)
+    .type(faker.internet.email())
+    .blur()
+    cy.findByText(/campo email inválido/i).should('not.exist')
+    cy.findByPlaceholderText(/email/i)
+    .type(faker.internet.password())
+    .blur()
+    cy.findByText(/o campo precisa ter no minimo 5 caracteres/i).should('not.exist')
+  })
+})
