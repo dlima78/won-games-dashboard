eed1ec387f835128bf48cbe70e1ee6104d61fc35
Eduardo Lima <dlima78@hotmail.com>
2022-03-11 16:56:43 -0300

    refactor: change length required in field name

--- src/main/factories/validation/sign-up-validation-factory.ts
+++ src/main/factories/validation/sign-up-validation-factory.ts
@@ -2,7 +2,7 @@ import { ValidationBuilder as Builder } from '@/main/builders'
 import { ValidationComposite } from '@/main/composite'
 
 export const makeSignUpValidation = (): ValidationComposite => ValidationComposite.build([
-  ...Builder.field('name').required().min(5).build(),
+  ...Builder.field('name').required().min(3).build(),
   ...Builder.field('email').required().email().build(),
   ...Builder.field('password').required().min(5).build(),
   ...Builder.field('passwordConfirmation').required().sameAs('password').build()
--- tests/main/factories/sign-up-validation-factory.spec.ts
+++ tests/main/factories/sign-up-validation-factory.spec.ts
@@ -7,7 +7,7 @@ describe('SignUpValidationFactory', () => {
     const composite = makeSignUpValidation()
 
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('name').required().min(3).build(),
       ...Builder.field('email').required().email().build(),
       ...Builder.field('password').required().min(5).build(),
       ...Builder.field('passwordConfirmation').required().sameAs('password').build()
